4745b6bc8dee144efc64933a342261bf67ef3905
tefadistyan <dystian_en_27rpl@student.smktelkom-mlg.sch.id>
2020-01-23 13:53:54 +0700

    List Project Beon

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
+class UserController extends Controller
+{
+    public function login(Request $request){
+		$credentials = $request->only('email', 'password');
+
+		try {
+			if(!$token = JWTAuth::attempt($credentials)){
+				return response()->json([
+						'logged' 	=>  false,
+						'message' 	=> 'Invalid email or password'
+					]);
+			}
+		} catch(JWTException $e){
+			return response()->json([
+						'logged' 	=> false,
+						'message' 	=> 'Generate Token Failed'
+					]);
+		}
+
+		return response()->json([
+					"logged"    => true,
+                    "token"     => $token,
+                    "message" 	=> 'Login berhasil'
+		]);
+    }
+    
+    public function register(Request $request)
+	{
+		$validator = Validator::make($request->all(), [
+			'firstname'      => 'required|string|max:255',
+			'lastname'      => 'required|string|max:255',
+			'email'     => 'required|string|email|max:255|unique:users',
+			'password'  => 'required|string|min:6',
+			'is_freelancer'  => 'required|string|max:255',
+			'is_productowner'  => 'required|string|max:255',
+			'address'  => 'required|string|max:255',
+		]);
+
+		if($validator->fails()){
+			return response()->json([
+				'status'	=> 0,
+				'message'	=> $validator->errors()->toJson()
+			]);
+		}
+            $user = new User();
+            $user->firstname 	= $request->firstname;
+            $user->lastname 	= $request->lastname;
+            $user->email 	= $request->email;
+            $user->password = Hash::make($request->password);
+            $user->is_freelancer 	= $request->is_freelancer;
+            $user->is_productowner 	= $request->is_productowner;
+            $user->address 	= $request->address;
+            $user->registered_at = now();
+            $user->save();
+
+            $token = JWTAuth::fromUser($user);
+            return response()->json([
+                'status'	=> '1',
+                'message'	=> 'User berhasil ditambahkan'
+            ], 201);
+	}
+	public function index($limit = 10, $offset = 0)
+	{
+		$data["count"] = User::count();
+		$user = array();
+		foreach (User::take($limit)->skip($offset)->get() as $p) {
+			$item = [
+				"id"             => $p->id,
+				"firstname"      => $p->firstname,
+				"lastname"  	 => $p->lastname,
+                "email"          => $p->email,
+                "is_freelancer"  => $p->is_freelancer,
+                "is_productowner"=> $p->is_productowner,
+                "address"        => $p->address,
+				"registered_at"  => $p->registered_at,
+				"updated_at"     => $p->updated_at,
+			];
+
+			array_push($user, $item);
+		}
+		$data["User"] = $user;
+		$data["status"] = 1;
+		return response($data);
+	}
+	public function update(Request $request)
+	{
+		$validator = Validator::make($request->all(), [
+			'firstname'      => 'required|string|max:255',
+			'lastname'       => 'required|string|max:255',
+			'email'          => 'required|string|email|max:255|unique:users',
+			'password'       => 'required|string|min:6',
+			'is_freelancer'  => 'required|string|max:255',
+			'is_productowner'=> 'required|string|max:255',
+			'address'        => 'required|string|max:255',
+		]);
+
+		if($validator->fails()){
+			return response()->json([
+				'status'	=> '0',
+				'message'	=> $validator->errors()
+			]);
+		}
+
+		//proses update data
+		$user = User::where('id', $request->id)->first();
+		$user->firstname 	= $request->firstname;
+        $user->lastname 	= $request->lastname;
+        $user->email 	= $request->email;
+        $user->password = Hash::make($request->password);
+        $user->is_freelancer 	= $request->is_freelancer;
+        $user->is_productowner 	= $request->is_productowner;
+        $user->address 	= $request->address;
+        $user->save();
+
+
+		return response()->json([
+			'status'	=> '1',
+			'message'	=> 'User berhasil diubah'
+		], 201);
+	}
+    public function getAuthenticatedUser(){
+		try {
+			if(!$user = JWTAuth::parseToken()->authenticate()){
+				return response()->json([
+						'auth' 		=> false,
+						'message'	=> 'Invalid token'
+					]);
+			}
+		} catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e){
+			return response()->json([
+						'auth' 		=> false,
+						'message'	=> 'Token expired'
+					], $e->getStatusCode());
+		} catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e){
+			return response()->json([
+						'auth' 		=> false,
+						'message'	=> 'Invalid token'
+					], $e->getStatusCode());
+		} catch (Tymon\JWTAuth\Exceptions\JWTException $e){
+			return response()->json([
+						'auth' 		=> false,
+						'message'	=> 'Token absent'
+					], $e->getStatusCode());
+		}
+
+		 return response()->json([
+		 		"auth"      => true,
+                "user"    	=> $user
+		 ], 201);
+	}
+	public function delete($id)
+    {
+        try{
+
+            User::where("id", $id)->delete();
+
+            return response([
+            	"status"	=> 1,
+                "message"   => "Data berhasil dihapus."
+            ]);
+        } catch(\Exception $e){
+            return response([
+            	"status"	=> 0,
+                "message"   => $e->getMessage()
+            ]);
+        }
+    }
+}
